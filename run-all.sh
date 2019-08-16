0dabc73fbd3101e5538580d3bfae7793917ae93b
Brian J. Cardiff <bcardiff@gmail.com>
2019-08-16 16:02:25 -0300

    Add compile hello_world

--- run-all.sh
+++ run-all.sh
@@ -7,3 +7,4 @@ benchy run -v --ndjson=$folder/channel-primes.ndjson channel-primes/run.yaml
 benchy run -v --ndjson=$folder/matmul.ndjson matmul/run.yaml
 benchy run -v --ndjson=$folder/matmul-channel.ndjson matmul-channel/run.yaml
 benchy run -v --ndjson=$folder/hello-world-http-server.ndjson hello-world-http-server/run.yaml
+benchy run -v --ndjson=$folder/compile.ndjson compile/run.yaml
