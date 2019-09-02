b3148facf25cf8adcf244e160dd36c45d62405f7
Brian J. Cardiff <bcardiff@gmail.com>
2019-09-02 10:42:59 -0300

    Keep logs on run-all

--- run-all.sh
+++ run-all.sh
@@ -3,9 +3,9 @@
 folder="output-$(date +%Y%m%d%H%M%S)"
 mkdir $folder
 
-benchy run -v --ndjson=$folder/channel-primes.ndjson channel-primes/run.yaml
-benchy run -v --ndjson=$folder/matmul.ndjson matmul/run.yaml
-benchy run -v --ndjson=$folder/matmul-channel.ndjson matmul-channel/run.yaml
-benchy run -v --ndjson=$folder/hello-world-http-server.ndjson hello-world-http-server/run.yaml
-benchy run -v --ndjson=$folder/hello-world-db-http-server.ndjson hello-world-db-http-server/run.yaml
-benchy run -v --ndjson=$folder/compile.ndjson compile/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/channel-primes.ndjson channel-primes/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/matmul.ndjson matmul/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/matmul-channel.ndjson matmul-channel/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/hello-world-http-server.ndjson hello-world-http-server/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/hello-world-db-http-server.ndjson hello-world-db-http-server/run.yaml
+benchy run --keep-logs -v --ndjson=$folder/compile.ndjson compile/run.yaml
